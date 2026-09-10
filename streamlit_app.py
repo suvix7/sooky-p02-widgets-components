a307a9fdcbd60bbb038704866aeea6bae4d00cc9
Szilard Sooky <suvix7@gmail.com>
2026-09-10 14:59:40 +0200

    Day1 update

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,69 +8,109 @@ st.write(
     "# 🤯 1. Nadpis (Title)"
 )
 
-
+st.title("Moja aplikacia s Widgetmi a komponentami")
 
 st.write(
     "# 🔠 2. Text (Text)"
 )
 
+st.write("Toto je klasicky test")
 
 
 st.write(
     "# ▶️ 3. Tlačidlo (Button)"
 )
 
-
+if st.button("Klikni na mna"):
+    st.write("Si klikol na tlacidlo")
 
 st.write(
     "# 🛝 4. Posuvník (slider)"
 )
 
+cislo = st.slider("Vyber cislo", 0, 100, 50)
+st.write(f"Vybrali ste cislo {cislo}")
 
 
 st.write(
     "# ✍ 5. Textový vstup (Text Input)"
 )
 
-
+meno = st.text_input("Zadajte svoje meno: ")
+st.write(f"Ahoj, {meno}")
 
 st.write(
     "# ☑️ 6. Zaškrtávacie políčko. (Checkbox)"
 )
 
-
+akcia = st.checkbox("Zobrazit text")
+if akcia:
+    st.write("Text je zobrazeny")
 
 st.write(
     "# 🔽 8. Rozbaľovací zoznam s možnosťami. (Selectbox)"
 )
 
-
+volba = st.selectbox("Vyberte moznost: ", ["Moznost 1", "Moznost 2", "Moznost 3"])
+st.write(f"Vybrali ste: {volba}")
 
 st.write(
     "# 📁 9. Upload súborov. (File uploader)"
 )
 
+subor = st.file_uploader("Nahrajte subor")
+if subor is not None:
+    st.write("Subor nahrany!")
 
 st.write(
     "# 🔥 10. Upload CSV súborov. (File CSV uploader)"
 )
 
+upload_subor = st.file_uploader("Nahrajte CVS subor: ", type="csv")
+if upload_subor is not None:
+    df = pd.read_csv(upload_subor)    
+    st.write("Subor nahrany!")
+    st.dataframe(df)
 
 st.write(
     "# 🏁 11. Upload obrázkov. (File image uploader)"
 )
 
-
+upload_obr = st.file_uploader("Nahrajte obrazok: ", type=["jpg", "png"])
+if upload_obr is not None:
+    obrazok = Image.open(upload_obr)
+    st.image(obrazok, caption = "Nahrany obrazok", use_container_width=True)
 
 st.write(
     "# 📥 12. Tlačidlo na stiahnutie. (Download button)"
 )
 
-
+# Konvertovanie pandas dataframe do CSV
+try:
+    csv = df.to_csv(index=False)
+    st.download_button(
+        label="Stiahnut CSV", 
+        data=csv, 
+        file_name="filmy.csv", 
+        mime="text/csv"
+        )
+except:
+    st.write("Nic na stiahnutie!")
 
 st.write(
     "# 📻 13. Výber jednej možnosti. (Radio)"
 )
+
+volba = st.radio("Valaszd ki a kedvenc gyikodat: ", ("Fürge gyík","Hidasgyík","Baziliszkusz gyík"))
+if volba == "Fürge gyík":
+    obr1 = Image.open("data\\furgegyik.jpg")
+    st.image(obr1, caption = "Fürge gyík", use_container_width=True)
+elif volba == "Hidasgyík":
+    obr1 = Image.open("data\\Hidasgyik.jpg")
+    st.image(obr1, caption = "Hidasgyík", use_container_width=True)
+else:
+    obr1 = Image.open("data\\Baziliszkuszgyik.jpg")
+    st.image(obr1, caption = "Baziliszkusz gyík", use_container_width=True)
 
 st.write(
     "# 📅 14. Výber dátumu. (Date input)"
